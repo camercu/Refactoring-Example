4b397bae250ec6a39de23509176f7f3ceb919281
camercu <cameron.unterberger@gmail.com>
2023-12-25 21:56:47 -0600

    refactor: rename function

--- src/refact/statement.py
+++ src/refact/statement.py
@@ -37,7 +37,7 @@ def statement(invoice: dict, plays: dict):
             result += volume_credits_for(perf)
         return result
 
-    def applesauce():
+    def total_amount():
         total_amount = 0
         for perf in invoice["performances"]:
             total_amount += amount_for(perf)
@@ -51,6 +51,6 @@ def statement(invoice: dict, plays: dict):
         # print line for this order
         result += f"  {play_for(perf)['name']}: {usd(amount_for(perf))} ({perf['audience']} seats)\n"
 
-    result += f"Amount owed is {usd(applesauce())}\n"
+    result += f"Amount owed is {usd(total_amount())}\n"
     result += f"You earned {total_volume_credits()} credits\n"
     return result
