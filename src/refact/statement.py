59869d16660a8adbd2211590c2ce6be1dafb9219
camercu <cameron.unterberger@gmail.com>
2023-12-25 23:18:23 -0600

    feature: add html statement

--- src/refact/statement.py
+++ src/refact/statement.py
@@ -7,6 +7,23 @@ def statement(invoice: dict, plays: dict):
     return render_plaintext(create_statement_data(invoice, plays))
 
 
+def html_statement(invoice: dict, plays: dict):
+    return render_html(create_statement_data(invoice, plays))
+
+
+def render_html(data):
+    result = f"<h1>Statement for {data['customer']}</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>"
+    for perf in data["performances"]:
+        result += f"  <tr><td>{perf['play']['name']}</td><td>{perf['audience']}</td>"
+        result += f"<td>{usd(perf['amount'])}</td></tr>\n"
+    result += "</table>\n"
+    result += f"<p>Amount owed is <em>${usd(data['totalAmount'])}</em></p>\n"
+    result += f"<p>You earned <em>${data['totalVolumeCredits']}</em> credits</p>\n"
+    return result
+
+
 def render_plaintext(data: dict):
     result = f"Statement for {data['customer']}\n"
     for perf in data["performances"]:
