a4e7bc06b4fb54a61c61fb5276b531f3549bb7b2
camercu <cameron.unterberger@gmail.com>
2023-12-25 23:07:11 -0600

    Move usd function to top level

--- src/refact/statement.py
+++ src/refact/statement.py
@@ -54,13 +54,14 @@ def statement(invoice: dict, plays: dict):
 
 
 def render_plaintext(data: dict):
-    def usd(cents):
-        locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
-        return locale.currency(cents / 100, grouping=True)
-
     result = f"Statement for {data['customer']}\n"
     for perf in data["performances"]:
         result += f"  {perf['play']['name']}: {usd(perf['amount'])} ({perf['audience']} seats)\n"
     result += f"Amount owed is {usd(data['total_amount'])}\n"
     result += f"You earned {data['total_volume_credits']} credits\n"
     return result
+
+
+def usd(cents):
+    locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+    return locale.currency(cents / 100, grouping=True)
